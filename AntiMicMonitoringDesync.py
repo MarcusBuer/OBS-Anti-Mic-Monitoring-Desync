8419ba2090c38418c49b73a89931f193788375a5
Bruno Nascimento <sputnicker@gmail.com>
2022-01-25 17:50:54 -0300

    Remove need to have an array of allowed audio APIs

--- AntiMicMonitoringDesync.py
+++ AntiMicMonitoringDesync.py
@@ -3,7 +3,6 @@ import obspython as obs
 ######## Global Variables ########
 source_name = ""
 update_interval = 60
-mic_types = ["wasapi_input_capture", "alsa_input_capture", "pulse_input_capture"]
 
 ######## Config Variables ########
 min_slider_value = 1
@@ -45,7 +44,7 @@ def script_load(settings):
 
 def script_properties():
   props = obs.obs_properties_create()
-  p = obs.obs_properties_add_list(props, "source", "Microphone", 
+  p = obs.obs_properties_add_list(props, "source", "Device", 
     obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_STRING)
   obs.obs_properties_add_int_slider(props, "update_interval", "Update Interval (s):", min_slider_value, max_slider_value, slider_inverval)
   sources = obs.obs_enum_sources()
@@ -53,11 +52,11 @@ def script_properties():
     for source in sources:
       source_id = obs.obs_source_get_id(source)
       print(f"Device type found: {source_id}.")
-      if source_id in mic_types:
+      if "input" in source_id:
         name = obs.obs_source_get_name(source)
         obs.obs_property_list_add_string(p, name, name)
   obs.source_list_release(sources)
   return props
 
 def script_description():
-  return "Resets the audio monitoring for the selected microphone to mitigate the buffer buildup problem."
+  return "Resets the audio monitoring for the selected device to mitigate the buffer buildup problem."

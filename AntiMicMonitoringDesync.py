05df7f5659c740255e914b98289f14b1bb4ee723
John <laubsterboy@users.noreply.github.com>
2022-02-11 08:12:51 -0700

    Update AntiMicMonitoringDesync.py
    Move the monitoring_type variable inside the if block.

--- AntiMicMonitoringDesync.py
+++ AntiMicMonitoringDesync.py
@@ -15,8 +15,8 @@ default_update_interval = 60
 def reset_monitoring():
   global source_name
   source = obs.obs_get_source_by_name(source_name)
-  monitoring_type = obs.obs_source_get_monitoring_type(source)
   if source:
+    monitoring_type = obs.obs_source_get_monitoring_type(source)
     obs.obs_source_set_monitoring_type(source, obs.OBS_MONITORING_TYPE_NONE)
     print("Monitoring disabled")
     obs.obs_source_set_monitoring_type(source, monitoring_type)
